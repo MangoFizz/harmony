03174700ad26819ce53a35e97494400056bfb3ed
JerryBrick <ccs81.mr@gmail.com>
2021-02-03 15:31:37 -0600

    Remove unneeded Lua stuff

--- ext/lua/lua.cmake
+++ ext/lua/lua.cmake
@@ -8,38 +8,46 @@ set(CMAKE_C_FLAGS "-Wall -Wextra")
 
 # Set Lua source files
 add_library(lua STATIC
+    # core
     ext/lua/lapi.c
-    ext/lua/lauxlib.c
-    ext/lua/lbaselib.c
-    ext/lua/lbitlib.c
     ext/lua/lcode.c
-    ext/lua/lcorolib.c
     ext/lua/lctype.c
-    ext/lua/ldblib.c
     ext/lua/ldebug.c
     ext/lua/ldo.c
     ext/lua/ldump.c
     ext/lua/lfunc.c
     ext/lua/lgc.c
-    ext/lua/linit.c
-    ext/lua/liolib.c
     ext/lua/llex.c
-    ext/lua/lmathlib.c
     ext/lua/lmem.c
-    ext/lua/loadlib.c
     ext/lua/lobject.c
     ext/lua/lopcodes.c
-    ext/lua/loslib.c
     ext/lua/lparser.c
     ext/lua/lstate.c
     ext/lua/lstring.c
-    ext/lua/lstrlib.c
     ext/lua/ltable.c
-    ext/lua/ltablib.c
     ext/lua/ltm.c
-    ext/lua/luac.c
     ext/lua/lundump.c
-    ext/lua/lutf8lib.c
     ext/lua/lvm.c
     ext/lua/lzio.c
+
+    # libraries
+    ext/lua/lauxlib.c
+    #ext/lua/lbaselib.c
+    #ext/lua/lbitlib.c
+    #ext/lua/lcorolib.c
+    #ext/lua/ldblib.c
+    #ext/lua/linit.c
+    #ext/lua/liolib.c
+    #ext/lua/lmathlib.c
+    #ext/lua/loadlib.c
+    #ext/lua/loslib.c
+    #ext/lua/lstrlib.c
+    #ext/lua/ltablib.c
+    #ext/lua/lutf8lib.c
+
+    # interpreter
+    #ext/lua/lua.c
+
+    # compiler
+    #ext/lua/luac.c
 )
