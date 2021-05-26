b4cd0691a7319709b4629a4c67e8b3805de7bf25
JerryBrick <ccs81.mr@gmail.com>
2021-05-26 00:08:26 -0500

    Add master volume data

---
+++ src/halo_data/master_volume.cpp
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+#include "../memory/signature.hpp"
+#include "../harmony.hpp"
+#include "master_volume.hpp"
+
+namespace Harmony::HaloData {
+    std::uint8_t get_master_volume() noexcept {
+        static auto &master_volume_sig = get_harmony().get_signature("master_volume");
+        static auto *master_volume = *reinterpret_cast<std::uint8_t **>(master_volume_sig.get_data()) + 0xB78;
+        return *master_volume;
+    }
+}
---
+++ src/halo_data/master_volume.hpp
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+#ifndef CHIMERA_PATH_HPP
+#define CHIMERA_PATH_HPP
+
+#include <cstdint>
+
+namespace Harmony::HaloData {
+    /**
+     * Get the master volume
+     */
+    std::uint8_t get_master_volume() noexcept;
+}
+
+#endif
