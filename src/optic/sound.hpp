9d8e4b03678119f8cc1c787eb4d262067d521da1
JerryBrick <ccs81.mr@gmail.com>
2021-05-26 00:13:57 -0500

    Add aliases for SoLoud stuff

---
+++ src/optic/sound.hpp
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+#ifndef HARMONY_OPTIC_SOUND_HPP
+#define HARMONY_OPTIC_SOUND_HPP
+
+#include <soloud.h>
+#include <soloud_wav.h>
+
+namespace Harmony::Optic {
+	using AudioEngine = SoLoud::Soloud;
+	using Sound = SoLoud::Wav;
+}
+
+#endif
